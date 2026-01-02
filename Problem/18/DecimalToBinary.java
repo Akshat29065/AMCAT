1db450ece58642c76c3863771014c647f825b390
Akshat Agarwal <akshat29605@gmail.com>
2026-01-02 16:42:56 +0530

    Decimal to Binary

---
+++ Problem/18/DecimalToBinary.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class DecimalToBinary {
+
+    public static int[] solution(int num){
+        List<Integer> binary = new ArrayList();
+        while(num > 0){
+            int temp = num % 2;
+            binary.add(temp);
+            num = (int) num/2;
+        }
+
+        Collections.reverse(binary);
+
+        int[] result = new int[binary.size()];
+
+        for(int i = 0; i < binary.size(); i++){
+            result[i] = binary.get(i);
+        }
+        return result;
+    }
+
+    public static String solution2(int num){
+        if(num == 0) return "0";
+
+        StringBuilder sb = new StringBuilder();
+
+        while(num > 0){
+            sb.append(num%2);
+            num = (int) num/2;
+        }
+
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        int num = 23;
+        System.out.println(Arrays.toString(solution(num)));
+        System.out.println(solution2(2));
+    }    
+}
