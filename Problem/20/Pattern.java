40b2057e7f4ac38000b0bee50c1aa703a7ff145b
Akshat Agarwal <akshat29605@gmail.com>
2026-01-02 18:09:02 +0530

    Pattern

---
+++ Problem/20/Pattern.java
@@ -0,0 +1,40 @@
+public class Pattern {
+
+    public static void pyramid(int n) {
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j ++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void dimond(int n) {
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j ++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = n - 1; i >= 1; i--){
+            for(int j = 1; j <= n - i; j ++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pyramid(5);
+        dimond(6);
+    }    
+}
