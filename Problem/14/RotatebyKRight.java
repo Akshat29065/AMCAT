7e7a06baafe85a4f54e2bff29369dde3279fe0ed
Akshat Agarwal <akshat29605@gmail.com>
2026-01-02 11:59:08 +0530

    array rotate by k right

---
+++ Problem/14/RotatebyKRight.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class RotatebyKRight {
+    public static int[] solution(int[] arr, int k){
+        int n = arr.length;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+        return arr;
+    }
+
+    public static void reverse(int[] arr, int left, int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int k = 2;
+        System.out.println(Arrays.toString(solution(arr, k)));
+    }    
+}
