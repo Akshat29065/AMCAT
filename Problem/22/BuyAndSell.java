2ec7780cfb124e4cab64af33a04aaae01d16d7b8
Akshat Agarwal <akshat29605@gmail.com>
2026-01-03 18:55:26 +0530

    Best time to buy and sell stock

---
+++ Problem/22/BuyAndSell.java
@@ -0,0 +1,19 @@
+public class BuyAndSell {
+
+    public static int solution(int[] arr){
+        int minprice = arr[0];
+        int profit = 0;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < minprice){
+                minprice = arr[i];
+            } else{
+                profit = Math.max(profit, arr[i] - minprice);
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(solution(arr));
+    }    
+}
